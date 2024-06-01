79ae8bb52dd83a99aae22bc8d55ae0e8e435f844
mechawolf-wd <home.mechawolf@gmail.com>
2024-06-01 22:31:17 +0200

    [General] Changed the name of Header component.

---
+++ components/AppHeader.js
@@ -0,0 +1,19 @@
+export const Template =/* HTML */ `
+  <h1 class="app-header">Vind</h1>
+`;
+
+export const AppHeader = () => { };
+
+export const Style = /* CSS */ `
+  .app-header {
+    background-color: #42b983;
+    color: #fff;
+    text-align: center;
+    padding: 1rem;
+    position: sticky;
+    top: 0;
+    z-index: 1000;
+    width: 100%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  }
+`;
